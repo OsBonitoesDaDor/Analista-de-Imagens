ef7ea619e65fd4472c68a25cd32a6a05637b984a
Bruno Pastre <pastre68@gmail.com>
2017-08-15 19:44:46 -0300

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,98 +1,103 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define PATH "C:\\Temp\\02.ppm"
+#define PATH "C:\\Temp\\10.ppm"
 #define FORMATO "P6"
 #define ERRO -1
 #define FALSE 0
 #define TRUE 1
-
-int init_val(){
-    int i = 0, j, k, wdt , hgt, num_linhas = 0, max_cor, pos_pxl = 0, lin = 0, col = 0;
+#define FUNDO '0'
+#define NAO_FUNDO ' '
+int init_val(unsigned char * * matriz, int * width, int * height) {
+	int i = 0, j, k, wdt, hgt, num_linhas = 0, max_cor, pos_pxl = 0, lin = 0, col = 0;
 	FILE * fp = NULL;
-    char linha[200], pxl_fundo[4], pxl_test[4];
-	unsigned char  * * matriz;
+	char linha[200], r_fundo, b_fundo, g_fundo, r_teste, g_teste, b_teste;
+	//unsigned char  * * matriz;
 	char * bora;
 
 	unsigned char c;
 
-	fp = fopen(PATH, "r");
+	fp = fopen(PATH, "rb");
 
-	if(fp == NULL){
+	if (fp == NULL) {
 		printf("DEU BO");
 		return 0;
 	}
+	do {
+		c = (unsigned char)fgetc(fp);
+		if (c != '\n') {
+			linha[i] = c;
+			i++;
+		}
+		else {
+			linha[i] = '\0';
+			switch (num_linhas) {
+			case 0:
+				if (strcmp(FORMATO, linha) != 0) {
+					printf("DEU BO NO FORMATO");
+					return ERRO;
+				}
+				break;
+			case 1:
+				hgt = atoi(strtok(linha, " "));
+				wdt = atoi(strtok(NULL, " "));
+				matriz = (unsigned char * *)malloc(sizeof(unsigned char *) *  wdt);
+				for (i = 0; i < wdt; i++)
+					matriz[i] = (unsigned char *)malloc(sizeof(unsigned char) * hgt);
+				break;
+			case 2:
+				max_cor = atoi(linha);
+				break;
+			}
+			num_linhas++;
+			i = 0;
+		}
+	} while (num_linhas < 3);
 
-	do{
-        c = (unsigned char ) fgetc(fp);
-        if(num_linhas < 3){
-            if(c != '\n'){
-                linha[i] = c;
-                i++;
-            }else{
-                linha[i] = '\0';
-                switch(num_linhas){
-                case 0:
-                    if(strcmp(FORMATO, linha) != 0){
-                        printf("DEU BO NO FORMATO");
-                        return ERRO;
-                    }
-                    break;
-                case 1:
-                    wdt = atoi(strtok( linha, " " ) );
-                    hgt = atoi(strtok( NULL, " " ) ) ;
-                    matriz = (unsigned char * * ) malloc(sizeof(unsigned char *) *  wdt);
-                    for(i = 0; i < wdt; i++)
-                        matriz[i] = ( unsigned char * ) malloc (sizeof(unsigned char) * hgt);
-                    break;
-                case 2:
-                    max_cor = atoi(linha);
-                    break;
-                }
-                num_linhas++;
-                i = 0;
-            }
-        //Concatena essa porra
-        }else{
-          //  printf("%s %s\n", pxl_fundo, pxl_test);
-          //TODO: mUDAR PARA FSCANF("%C, %C, %C", A, B, C"; APRA LER DE 3 EM 3!!
-            if(i < 3){
-                pxl_fundo[i] = c;
-                i++;
-            }else if(j < 3){
-                pxl_test[j] = c;
-                j++;
-            }else{
-                if(strcmp(pxl_test, pxl_fundo) == 0){
-                    matriz[lin][col] = 0;
-                }else{
-                    matriz[lin][col] = 1;
-                }
-                j = 0;
-                if(col == hgt){
-                    lin++;
-                    col = 0;
-                }else{
-                    col++;
-                }
+	printf("A imagem eh %d %d \n", wdt, hgt);
 
-        }
-        }
-        if(lin == wdt)
-            break;
-	}while(c != 'EOF' );
+	for (lin = 0; lin < wdt; lin++) {
+		for (col = 0; col < hgt; col++) {
+			if ((lin == 0) && (col == 0)) {
+				fscanf(fp, "%c%c%c", &r_fundo, &g_fundo, &b_fundo);
+				matriz[0][0] = FUNDO;
+			}
+			else
+				fscanf(fp, "%c%c%c", &r_teste, &g_teste, &b_teste);
 
-	printf("VALORES INICIADS!!\n\n");
-
-	for(i = 0; i < wdt; i ++){
-        for(j = 0; j < hgt; j++){
-        //printf("%d ", matriz[i][j] -1 );
-        }
-        //printf("\n");
+			if ((r_fundo != r_teste) || (g_fundo != g_teste) || (b_fundo != b_teste))
+				matriz[lin][col] = NAO_FUNDO;
+			else
+				matriz[lin][col] = FUNDO;
+		}
 	}
+	matriz[0][0] = FUNDO;
+
+	
+
+	//mat_res = matriz;
+	*width = wdt;
+	*height = hgt;
 }
 
-int main(){
-    init_val();
+int main() {
+	unsigned char * * matriz = NULL;
+	int wdt, hgt, lin, col;
+	init_val(matriz, &wdt, &hgt);
+
+	printf("aaaa %d %d \n", wdt, hgt);
+
+	for (lin = 0; lin < wdt; lin++) {
+		for (col = 0; col < hgt; col++) {
+			printf("%c", matriz[lin][col]);
+		}
+		printf("\n");
+	}
+
+	system("PAUSE");
+	return 0;
 }
